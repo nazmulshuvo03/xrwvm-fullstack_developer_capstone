6b1dfaee876a77e7c5ac1df9d5c1b83770fa74e7
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-12-24 02:12:41 +0600

    new app link updated

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,9 +31,11 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "https://nazmulshuvo0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://nazmulshuvo0-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://nazmulshuvo0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"
+    "https://nazmulshuvo0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://nazmulshuvo0-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
 
 REST_FRAMEWORK = {
