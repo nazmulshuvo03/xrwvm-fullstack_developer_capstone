3127dec6884c9752b97c4566e14e5897128fea58
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-12-23 19:56:08 +0600

    logout created

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # # path for registration
     # path for login
     path(route="login", view=views.login_user, name="login"),
+    # path for logout
+    path(route="logout", view=views.logout_request, name="logout"),
     # path for dealer reviews view
     # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -42,8 +42,11 @@ def login_user(request):
 
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName": ""}
+    return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
