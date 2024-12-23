c45df3dad6ff5f0c99bf1451bb7ade98ad8bd594
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-12-23 20:12:02 +0600

    register user added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path(route="login", view=views.login_user, name="login"),
     # path for logout
     path(route="logout", view=views.logout_request, name="logout"),
+    # path for registration
+    path(route="register", view=views.registration, name="register"),
     # path for dealer reviews view
     # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
